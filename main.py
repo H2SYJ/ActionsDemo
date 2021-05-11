7f007208023b139a535f3723f704dbd22c11e1fb
H2SYJ <63276373+H2SYJ@users.noreply.github.com>
2021-05-11 20:27:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import datetime
 
 book_id = '4305547728'
 url = "http://dushu.baidu.com/api/pc/getDetail?data={%22book_id%22:%22"+book_id+"%22}"
@@ -12,6 +13,8 @@ if __name__ == '__main__':
     book_name = j['data']['novel']['book_name']#书名
     book_title = j['data']['novel']['lastChapter']['chapter_title']#章节名
     update_time = j['data']['novel']['lastChapter']['cp_update_time']#更新时间
+    dateArray = datetime.datetime.utcfromtimestamp(int(update_time))
+    update_time = dateArray.strftime("%Y-%m-%d %H:%M:%S")
     print("书名："+book_name)
     print("标题："+book_title)
     print("更新时间："+update_time)
