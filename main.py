994ddcae9c0036153db3b3a6f209900e98d16e97
H2SYJ <63276373+H2SYJ@users.noreply.github.com>
2021-05-11 20:35:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     print("标题："+book_title)
     print("更新时间："+update_time)
     now = datetime.datetime.now()
-    print(now.strftime("%Y-%m-%d"))
+    print("当前时间："+now.strftime("%Y-%m-%d %H:%M:%S"))
     if (now.strftime("%Y-%m-%d") == dateArray.strftime("%Y-%m-%d")):
             print('send mail')
             #发送邮件
